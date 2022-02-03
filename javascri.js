a0bb8cf757d77c4f0b93a91eeb48c91da51fe819
asheeshjaat <98040615+asheeshjaat@users.noreply.github.com>
2022-02-03 14:54:31 +0530

    Add files via upload

---
+++ javascri.js
@@ -0,0 +1,12 @@
+var navlinks = document.getElementById("navlinks");
+
+
+function showmenu(){
+navlinks.style.right = "0";
+}
+
+function hidemenu(){
+  navlinks.style.right = "-200px";
+}
+showmenu();
+hidemenu();
